b39e439b6749e79a73aa2d0bc20c8c38d7283ad7
GC3KInoue <71299917+GC3KInoue@users.noreply.github.com>
2023-01-10 20:43:02 +0900

    muzukasikune
    muzukasikune

---
+++ My project/Assets/Field/NormalField.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalField : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // ログを表示
+        Debug.Log("スクリプトが呼び出されました!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
